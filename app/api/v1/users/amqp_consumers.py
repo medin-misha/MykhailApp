b73edf9bbad2b2939b46ecdce29a2269d5536221
misha <medinskijmisa228@gmail.com>
2025-09-25 15:47:28 +0200

    Added set_user_birthday_hander and added str_in_date in utils.py

--- app/api/v1/users/amqp_consumers.py
+++ app/api/v1/users/amqp_consumers.py
@@ -2,9 +2,9 @@ from faststream.rabbit.broker import RabbitBroker
 from pydantic import ValidationError
 from config import settings
 from core.database import database
-from contracts.user import ReturnUser, CreateUser
+from contracts.user import ReturnUser, CreateUser, BirthdayModel
 from services.user import UserCrud
-from .utils import str_in_dict_or_error_log
+from .utils import str_in_dict_or_error_log, str_in_date
 
 broker = RabbitBroker(url=settings.rabbit_url)
 
@@ -27,3 +27,30 @@ async def order_queue_handler(data: str): # data: CreateUser
         return
     async with database.session_maker() as session:
         await UserCrud.get_or_create(data=user, session=session)
+
+
+@broker.subscriber(
+    "user_birthday",
+    description=(
+        "Хендлер подписан на очередь `auth_birthday`. "
+        "Принимает данные пользователя в формате JSON (строка), "
+        "конвертирует их в dict и валидирует через модель `BirthdayModel`. "
+        "После успешной валидации обновляет дату рождения пользователя "
+        "в базе данных с помощью метода `UserCrud.update`."
+    ),
+)
+async def set_user_birthday_handler(data: str): # data: BrithdayModel
+    try:
+        payload = str_in_dict_or_error_log(data=data)
+        birthday_data = BirthdayModel(**payload)
+    except ValidationError as e:
+        print(f"Ошибка валидации данных: {e}")
+        return
+    async with database.session_maker() as session:
+        await UserCrud.update(
+            chat_id=birthday_data.chat_id,
+            field="birthday_date",
+            session=session,
+            new_value=str_in_date(birthday_data.birthday)
+        )
+
--- app/api/v1/users/utils.py
+++ app/api/v1/users/utils.py
@@ -1,3 +1,4 @@
+from datetime import datetime, date
 import json
 
 def str_in_dict_or_error_log(data: str) -> dict | None:
@@ -7,3 +8,9 @@ def str_in_dict_or_error_log(data: str) -> dict | None:
         print(f"Ошибка в пришли некорректные данные, не JSON: {data}")
         return None
     return payload
+
+def str_in_date(date_str: str) -> date:
+    try:
+        return datetime.strptime(date_str, "%d/%m/%Y").date()
+    except ValueError:
+        raise ValueError("Дата должна быть в формате dd/mm/YYYY")
