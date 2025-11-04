e6a74b25ae48a513abc8db2bf012d68920e68de3
misha <medinskijmisa228@gmail.com>
2025-11-04 07:10:52 +0100

    deleted registered_at, last_login_at, is_active, source in User

--- app/contracts/user/schemas.py
+++ app/contracts/user/schemas.py
@@ -7,19 +7,26 @@ from contracts.subscriptions import SubscribeUserReturn
 # ---------- Базовая модель ----------
 class UserBase(BaseModel):
     chat_id: int = Field(..., description="Telegram chat_id пользователя")
-    username: Optional[str] = Field(None, max_length=64, description="Имя пользователя в Telegram")
-    phone: Optional[str] = Field(None, max_length=15, description="Номер телефона пользователя")
-    email: Optional[EmailStr] = Field(None, description="Электронная почта пользователя")
-    birthday_date: Optional[date] = Field(None, description="Дата рождения пользователя")
-    is_active: bool = Field(default=True, description="Активен ли пользователь")
-    source: Optional[str] = Field(None, description="Место откуда пришел пользователь")
-    language: Optional[str] = Field(None, description="Язык на котором разговаривает пользователь")
+    username: Optional[str] = Field(
+        None, max_length=64, description="Имя пользователя в Telegram"
+    )
+    phone: Optional[str] = Field(
+        None, max_length=15, description="Номер телефона пользователя"
+    )
+    email: Optional[EmailStr] = Field(
+        None, description="Электронная почта пользователя"
+    )
+    birthday_date: Optional[date] = Field(
+        None, description="Дата рождения пользователя"
+    )
+
 
 # ---------- Модель создания ----------
 class UserCreate(UserBase):
     """
     Используется при создании пользователя (например, при первом логине через Telegram)
     """
+
     pass
 
 
@@ -28,12 +35,11 @@ class UserUpdate(BaseModel):
     """
     Используется для обновления данных пользователя
     """
+
     username: Optional[str] = Field(None, max_length=64)
     phone: Optional[str] = Field(None, max_length=15)
     email: Optional[EmailStr] = None
     birthday_date: Optional[date] = None
-    is_active: Optional[bool] = None
-    last_login_at: Optional[datetime] = None
 
 
 # ---------- Модель возврата ----------
@@ -41,13 +47,15 @@ class UserReturn(UserBase):
     """
     Используется для возврата данных пользователю (в ответах API)
     """
-    id: int
-    registered_at: datetime
-    last_login_at: Optional[datetime] = None
 
+    id: int
     # если нужно показать связанные сущности (например, списки платежей и подписок)
-    subscriptions: Optional[List[SubscribeUserReturn]] = Field(default_factory=list, description="ID подписок пользователя")
-    payments: Optional[List[int]] = Field(default_factory=list, description="ID платежей пользователя")
+    subscriptions: Optional[List[SubscribeUserReturn]] = Field(
+        default_factory=list, description="ID подписок пользователя"
+    )
+    payments: Optional[List[int]] = Field(
+        default_factory=list, description="ID платежей пользователя"
+    )
 
     class Config:
         from_attributes = True  # (Pydantic v2) аналог orm_mode=True
--- app/core/models/user.py
+++ app/core/models/user.py
@@ -1,42 +1,39 @@
 from __future__ import annotations
 
 import enum
-from datetime import date, datetime
+from datetime import date
 from typing import Optional
 
-from sqlalchemy import BigInteger, Date, DateTime, String, Boolean, func
+from sqlalchemy import BigInteger, Date, String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from sqlalchemy import Enum as SQLEnum
 
 from .base import Base
+
 
 class LanguageEnum(str, enum.Enum):
     """
     Язык пользователь (в дальнейшем язык интерфейса)
     """
+
     EN = "en"
     RU = "ru"
+
 
 class User(Base):
     """
     Пользователь, ключевая сущность для аутентификации по Telegram chat_id
     """
+
     id: Mapped[int] = mapped_column(BigInteger, primary_key=True, index=True)
-    chat_id: Mapped[int] = mapped_column(BigInteger, unique=True, index=True, nullable=False)
-    username: Mapped[Optional[str]] = mapped_column(String(64),  nullable=True)
+    chat_id: Mapped[int] = mapped_column(
+        BigInteger, unique=True, index=True, nullable=False
+    )
+    username: Mapped[Optional[str]] = mapped_column(String(64), nullable=True)
     phone: Mapped[Optional[str]] = mapped_column(String(15), nullable=True)
     email: Mapped[Optional[str]] = mapped_column(String(256), nullable=True)
     birthday_date: Mapped[Optional[date]] = mapped_column(Date, nullable=True)
-    is_active: Mapped[bool] = mapped_column(Boolean, nullable=False, default=True)
-    registered_at: Mapped[datetime] = mapped_column(
-        DateTime(timezone=True), server_default=func.now(), nullable=False
-    )
-    last_login_at: Mapped[Optional[datetime]] = mapped_column(DateTime(timezone=True))
-    source: Mapped[Optional[str]] = mapped_column(String(100))
     language: Mapped[str] = mapped_column(
-        nullable=True,
-        default=LanguageEnum.RU,
-        doc="Язык интерфейса пользователя"
+        nullable=True, default=LanguageEnum.RU, doc="Язык интерфейса пользователя"
     )
     # отношения
     subscriptions: Mapped[list["UserSubscription"]] = relationship(
